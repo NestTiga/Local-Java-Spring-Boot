26c67ed8016a3bdab4583625901cb0cda83a28e8
NestTiga <nestortigasi371@gmail.com>
2024-07-11 20:49:50 -0500

    creada la configuración para email

---
+++ src/main/java/com/tigasinestor/local/config/EmailConfig.java
@@ -0,0 +1,47 @@
+package com.tigasinestor.local.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:email.properties") // Se indica la ubicación del archivo de propiedades de correo
+public class EmailConfig {
+    @Value("${email.username}") // Se obtiene el valor de la propiedad email.username
+    private String emailUsername;
+
+    @Value("${email.password}") // Se obtiene el valor de la propiedad email.password
+    private String emailPassword;
+
+    private Properties getMailProperties() {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+
+        return properties;
+    }
+
+    @Bean
+    public JavaMailSender javaMailSender(){
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setJavaMailProperties(getMailProperties());
+        mailSender.setUsername(emailUsername);
+        mailSender.setPassword(emailPassword);
+        return mailSender;
+    }
+
+    @Bean
+    public ResourceLoader resourceLoader(){
+        return new DefaultResourceLoader();
+    }
+
+}
